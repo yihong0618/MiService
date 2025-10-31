1b4581d0af504075c6eb0404e4785a50a25428e5
yihong0618 <zouzou0208@gmail.com>
2025-10-31 22:19:13 +0800

    chore: bump version
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 setup(
     name="miservice_fork",
     description="XiaoMi Cloud Service fork from https://github.com/Yonsm/MiService",
-    version="2.9.0",
+    version="2.9.3",
     license="MIT",
     author="Yonsm, yihong0618",
     author_email="Yonsm@qq.com, zouzou0208@gmail.com",
